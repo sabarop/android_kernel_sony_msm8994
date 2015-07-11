652807727c31ad0b2a3c665dd9df5c7b7b8a2b94
Steve Kondik <steve@cyngn.com>
2015-07-11 05:49:30 -0700

    video: mdss: LiveDisplay driver
     * Provide a simple color temperature adjustment interface via MDSS PCC.
     * Provide hardware integration support for CABC, SRE, and CE.
    
    Change-Id: I4ed1f3876767dd6bedccaf0bbf61af7d36730ca0
    
    video: mdss: Add new features and improve LiveDisplay
    
     * Add support for auto contrast optimization.
     * Add preset support.
     * Presets are arbitrary sets of DSI commands (defined in the DT) which
       can be used for implementing device-specific modes.
     * This adds the "preset" and "num_presets" sysfs nodes.
     * The preset at position zero is always applied.
     * Completely reworked the way we send commands to the hardware. Now we
       create a single DSI command buffer and send the whole thing at once.
    
    Change-Id: I9b986593cf42b65a3360dae474d8318406b48204
    
    video: mdss: Cache LiveDisplay RGB settings when display off
    
     * We should avoid waking up the hardware if not necessary-
       but unfortunately even if we do, we might not get a valid
       result when querying for the current PCC settings.
     * Get rid of this code and keep track of the desired values
       ourselves, and reapply it during wakeup.
    
    Change-Id: I1d42842af0d044e241dcc5b9266be0db4f84800b
    
    video: mdss: Fix dual-DSI issue in LiveDisplay
    
     * The second controller won't have it's own framebuffer, so don't
       dereference a NULL pointer.
    
    Change-Id: I0d0c1475882eb65c8d5bc69c9ac3776b5d60339f
    
    video: mdss: Implement PCC combining for LiveDisplay
    
     * If we send calibration data via ioctl, we want to make sure
       it's preserved when applying RGB offsets via LiveDisplay.
     * Add support for combining the values, using the calibrated
       PCC value as a scaling factor.
    
    Change-Id: I33a5678a858f5e1c609709b4e7e901c792fd2590
    
    video: mdss: Simplify PCC combiner
    
     * Just combine the values instead of trying to scale things.
    
    Change-Id: I5e5b94ad117ec4c01d49fc38b1d967817de76825
    
    video: mdss: Simplify PCC combiner even more
    
     * We shouldn't return zero from this.
    
    Change-Id: I181a878d442e55e930c1a9532c40c84c3a9b1361
    
    video: mdss: Fix issues when setting a channel to zero
    
    Change-Id: Idfaca981712abbf5bfeee9404525d8776bff1cb9
    
    mdss: LiveDisplay: fix cherry-pick failure
    
    Change-Id: I9c454a54394b3a29420407e3d6866bc1447fd0fb
    
    msm: mdss: move mdss_livedisplay_update() to after tear configuration
    
    With livedisplay, it's the mdss-livedisplay-post-cmd sequence from the
    panel's device tree which finally enables the display/backlight. Tear
    effect configuration should happen before this. Move the call to
    mdss_livedisplay_update() so that it occurs after all other panel
    configuration has been completed.
    
    Change-Id: I62d847103a1a1df6a3013c7c3520d7283a4b422f
    Signed-off-by: Matt Wagantall <mwagantall@cyngn.com>

--- drivers/video/msm/mdss/mdss_dsi_panel_driver.c
+++ drivers/video/msm/mdss/mdss_dsi_panel_driver.c
@@ -39,6 +39,7 @@
 
 #include "mdss_mdp.h"
 #include "mdss_dsi.h"
+#include "mdss_livedisplay.h"
 
 #define DT_CMD_HDR 6
 #define MIN_REFRESH_RATE 30
@@ -252,7 +253,7 @@ u32 mdss_dsi_panel_cmd_read(struct mdss_dsi_ctrl_pdata *ctrl, char cmd0,
 	return 0;
 }
 
-static void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
+void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
 			struct dsi_panel_cmds *pcmds, u32 flags)
 {
 	struct dcs_cmd_req cmdreq;
@@ -1822,7 +1823,7 @@ static void mdss_dsi_parse_trigger(struct device_node *np, char *trigger,
 	}
 }
 
-static int mdss_dsi_parse_dcs_cmds(struct device_node *np,
+int mdss_dsi_parse_dcs_cmds(struct device_node *np,
 		struct dsi_panel_cmds *pcmds, char *cmd_key, char *link_key)
 {
 	const char *data;
@@ -3479,6 +3480,8 @@ static int mdss_panel_parse_dt(struct device_node *np,
 		rc = of_property_read_u32(np, "somc,poll-esd-reg-val", &tmp);
 		spec_pdata->polling.esd.correct_val = (!rc ? tmp : 0x9C);
 	}
+
+	mdss_livedisplay_parse_dt(np, pinfo);
 
 	return 0;
 
