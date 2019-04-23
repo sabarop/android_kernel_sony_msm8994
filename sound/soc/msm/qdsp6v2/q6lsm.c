8719163baf3f85726fc118cb380ba82c8626c28b
kunleiz <kunleiz@codeaurora.org>
2019-04-23 11:26:41 +0800

    lsm: check payload size validity before using it as array index
    Payload size validity is not checked before using it in array index.
    Check payload size to avoid out-of-boundary memory.
    
    Change-Id: Ic0b06bb331fc1753ff7543bb218ab12d6a4a3ca8
    Signed-off-by: Kunlei Zhang <kunleiz@codeaurora.org>
    CVE-2019-10512

--- sound/soc/msm/qdsp6v2/q6lsm.c
+++ sound/soc/msm/qdsp6v2/q6lsm.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2015, Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2016, 2019 Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -223,7 +223,7 @@ static int q6lsm_callback(struct apr_client_data *data, void *priv)
 
 	if (client->cb)
 		client->cb(data->opcode, data->token, data->payload,
-			   client->priv);
+				data->payload_size, client->priv);
 
 	return 0;
 }
@@ -1121,6 +1121,8 @@ static int q6lsm_mmapcallback(struct apr_client_data *data, void *priv)
 		pr_debug("%s: SSR event received 0x%x, event 0x%x,\n"
 			 "proc 0x%x SID 0x%x\n", __func__, data->opcode,
 			 data->reset_event, data->reset_proc, sid);
+		if (sid < LSM_MIN_SESSION_ID || sid > LSM_MAX_SESSION_ID)
+			pr_err("%s: Invalid session %d\n", __func__, sid);
 		lsm_common.common_client[sid].lsm_cal_phy_addr = 0;
 		cal_utils_clear_cal_block_q6maps(LSM_MAX_CAL_IDX,
 			lsm_common.cal_data);
@@ -1182,7 +1184,8 @@ static int q6lsm_mmapcallback(struct apr_client_data *data, void *priv)
 	}
 	if (client->cb)
 		client->cb(data->opcode, data->token,
-			   data->payload, client->priv);
+			data->payload, data->payload_size,
+			client->priv);
 	return 0;
 }
 
