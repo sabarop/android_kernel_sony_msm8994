c7458885caffad78c027eaa3baf3449958a53011
Sabar <31942277+sabarop@users.noreply.github.com>
2021-07-10 17:00:23 +0700

    Revert "mm/readahead.c:do_readhead(): don't check for ->readpage"
    This reverts commit 3532d9ceeeba70772b9a27605784e566023c13be.

--- mm/readahead.c
+++ mm/readahead.c
@@ -575,7 +575,7 @@ static ssize_t
 do_readahead(struct address_space *mapping, struct file *filp,
 	     pgoff_t index, unsigned long nr)
 {
-	if (!mapping || !mapping->a_ops)
+	if (!mapping || !mapping->a_ops || !mapping->a_ops->readpage)
 		return -EINVAL;
 
 	force_page_cache_readahead(mapping, filp, index, nr);
