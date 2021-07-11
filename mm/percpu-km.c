32658b1cb28766c09aff979a147d1e07a0080aa3
sabar <sabar.pakpahan@gmail.com>
2021-07-11 12:57:34 +0700

    Revert "percpu: make percpu-km set chunk->populated bitmap properly"
    This reverts commit 0d528aa4fc592dfee7ddda075eac0e3cc143a822.

--- mm/percpu-km.c
+++ mm/percpu-km.c
@@ -70,9 +70,6 @@ static struct pcpu_chunk *pcpu_create_chunk(void)
 
 	chunk->data = pages;
 	chunk->base_addr = page_address(pages) - pcpu_group_offsets[0];
-
-	bitmap_fill(chunk->populated, nr_pages);
-
 	return chunk;
 }
 
